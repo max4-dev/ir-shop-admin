6e2859ddd512c71c25e4f8785361a5bb62716723
Max <maksim.bogomyakov@gmail.com>
2024-07-14 12:21:01 +0300

    fix categories

--- src/components/layouts/category/CategoryCreate/CategoryCreate.tsx
+++ src/components/layouts/category/CategoryCreate/CategoryCreate.tsx
@@ -1,13 +1,36 @@
-import { Create, SimpleForm, TextInput } from "react-admin";
+import { Create, SelectInput, SimpleForm, TextInput, useGetList } from "react-admin";
+import { useEffect, useState } from "react";
 
-export const CategoryCreate = () => (
-  <Create>
-    <SimpleForm>
-      <TextInput source="name" label="Категория" />
-      <TextInput
-        source="parent"
-        label="Родительская категория (если нет, то сам становится родителем)"
-      />
-    </SimpleForm>
-  </Create>
-);
+import { defaultDataValues, formatDataOne } from "../../../../helpers/formatDataOne";
+
+export const CategoryCreate = () => {
+  const [formattedCategories, setFormattedCategories] = useState<unknown[]>([]);
+  const { data, isPending } = useGetList("category");
+
+  useEffect(() => {
+    const formatted = data
+      ? data
+          .map((category) => {
+            if (category.parent) {
+              return null;
+            }
+            return formatDataOne(category, ...defaultDataValues);
+          })
+          .filter(Boolean)
+      : [];
+    setFormattedCategories(formatted);
+  }, [data]);
+
+  return (
+    <Create>
+      <SimpleForm>
+        <TextInput source="name" label="Категория" />
+        <SelectInput
+          source="parent"
+          choices={formattedCategories}
+          label="Родительская категория (если нет, то сам становится родителем)"
+        />
+      </SimpleForm>
+    </Create>
+  );
+};
--- src/components/layouts/category/CategoryList/CategoryList.tsx
+++ src/components/layouts/category/CategoryList/CategoryList.tsx
@@ -1,11 +1,38 @@
-import { Datagrid, List, ReferenceField, TextField } from "react-admin";
+import { Datagrid, FunctionField, List, ReferenceField, TextField } from "react-admin";
+import { useEffect } from "react";
 
-export const CategoryList = () => (
-  <List>
-    <Datagrid rowClick="edit">
-      <ReferenceField source="id" reference="category" />
-      <TextField source="name" />
-      <TextField source="parent" />
-    </Datagrid>
-  </List>
-);
+import { useActions } from "../../../../hooks/useActions";
+import { useAppSelector } from "../../../../redux/store";
+import { ICategory } from "../../../../redux/category/types";
+
+export const CategoryList = () => {
+  const { getGategories } = useActions();
+  const { categories } = useAppSelector((state) => state.category);
+
+  useEffect(() => {
+    getGategories();
+  }, []);
+
+  const findCategoryById = (id: string | null) => {
+    const findedCategory = categories && categories.find((category) => category.id === id);
+
+    if (!findedCategory) {
+      return "";
+    }
+
+    return findedCategory.name;
+  };
+
+  return (
+    <List>
+      <Datagrid rowClick="edit">
+        <ReferenceField source="id" reference="category" />
+        <TextField source="name" label="Название" />
+        <FunctionField
+          label="Родитель"
+          render={(record: ICategory) => findCategoryById(record.parent)}
+        />
+      </Datagrid>
+    </List>
+  );
+};
